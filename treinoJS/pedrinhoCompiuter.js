6296a470defac21907df7672a82adef069deb306
Pedro Henrique Matos Dos Santos <pedro_matos2012@hotmail.com>
2023-04-27 12:37:07 -0300

    novo readline

--- treinoJS/pedrinhoCompiuter.js
+++ treinoJS/pedrinhoCompiuter.js
@@ -1,1 +1,11 @@
-console.log("Um olar como nenhum outro!");
+console.log("\nUm olar como nenhum outro!\n");
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+readline.question(`Escreva seu nome: `, nome => {
+    console.log(`\nOlá ${nome}!\n\nAperte [Ctrl + C] para sair`);
+    readline.close
+})
